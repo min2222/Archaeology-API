1fcf2a01c068be56e3470e64077044d84edf8e80
min <goyoumin2@gmail.com>
2023-12-31 16:35:37 +0900

    fixed #3

--- src/main/java/com/min01/archaeology/mixin/MixinOceanRuinPiece.java
+++ src/main/java/com/min01/archaeology/mixin/MixinOceanRuinPiece.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 
 import com.min01.archaeology.init.ArchaeologyBlocks;
 import com.min01.archaeology.init.ArchaeologyLootTables;
@@ -36,9 +35,6 @@ public class MixinOceanRuinPiece implements IOceanRuinPiece
 	@Shadow
 	private @Final OceanRuinStructure.Type biomeType;
 	
-	@Unique
-	private RandomSource random = RandomSource.create();
-	
 	private static final StructureProcessor WARM_SUSPICIOUS_BLOCK_PROCESSOR = archyRuleProcessor(Blocks.SAND, ArchaeologyBlocks.SUSPICIOUS_SAND.get(), ArchaeologyLootTables.OCEAN_RUIN_WARM_ARCHAEOLOGY);
 	private static final StructureProcessor COLD_SUSPICIOUS_BLOCK_PROCESSOR = archyRuleProcessor(Blocks.GRAVEL, ArchaeologyBlocks.SUSPICIOUS_GRAVEL.get(), ArchaeologyLootTables.OCEAN_RUIN_COLD_ARCHAEOLOGY);
 	
@@ -51,6 +47,6 @@ public class MixinOceanRuinPiece implements IOceanRuinPiece
 	public StructurePlaceSettings getPlaceSetting() 
 	{
 		StructureProcessor structureprocessor = this.biomeType == OceanRuinStructure.Type.COLD ? COLD_SUSPICIOUS_BLOCK_PROCESSOR : WARM_SUSPICIOUS_BLOCK_PROCESSOR;
-		return (new StructurePlaceSettings()).setRotation(Rotation.getRandom(this.random)).setMirror(Mirror.NONE).addProcessor(BlockIgnoreProcessor.STRUCTURE_AND_AIR).addProcessor(structureprocessor);
+		return (new StructurePlaceSettings()).setRotation(Rotation.getRandom(RandomSource.create())).setMirror(Mirror.NONE).addProcessor(BlockIgnoreProcessor.STRUCTURE_AND_AIR).addProcessor(structureprocessor);
 	}
 }
